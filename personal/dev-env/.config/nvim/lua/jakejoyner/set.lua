0a40e9dcfa0c712bc5382c6ade4f8f68e6301c66
jakejoyner <jakejoyner9@gmail.com>
2025-03-27 16:03:51 -0700

    add dev-env

---
+++ personal/dev-env/.config/nvim/lua/jakejoyner/set.lua
@@ -0,0 +1,34 @@
+vim.opt.guicursor = ""
+vim.g.have_nerd_font = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = vim.fn.stdpath("state") .. "/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scroll = 11
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+vim.opt.ttyfast = true
